03c8397beb8eaac3afff26030df91555b584f97b
Roman Voitenko <roman.voitenko@konsult.atg.se>
2023-05-27 11:08:26 +0200

    * added debug flag * interval converted to seconds

--- main.go
+++ main.go
@@ -15,7 +15,8 @@ import (
 )
 
 var (
-	version = "0.0.9"
+	version = "0.0.10"
+	debug   bool // Add this line for the debug flag
 )
 
 type Response []struct {
@@ -35,7 +36,8 @@ func doEvery(d time.Duration, f func(time.Time)) {
 
 func pushToInflux(t time.Time) {
 	httpClient := &http.Client{}
-	req, _ := http.NewRequest("GET", "https://www.vattenfall.se/api/price/spot/pricearea/"+time.Now().Format("2006-01-02")+"/"+time.Now().AddDate(0, 0, 1).Format("2006-01-02")+"/SN3", nil)
+	url := "https://www.vattenfall.se/api/price/spot/pricearea/" + time.Now().Format("2006-01-02") + "/" + time.Now().AddDate(0, 0, 1).Format("2006-01-02") + "/SN3"
+	req, _ := http.NewRequest("GET", url, nil)
 	req.Header.Set("User-Agent", fmt.Sprintf("vattenfall-to-influxdb/%s (+https://github.com/rvoitenko/vattenfall-to-influxdb)", version))
 	resp, err := httpClient.Do(req)
 	defer resp.Body.Close()
@@ -48,43 +50,58 @@ func pushToInflux(t time.Time) {
 		fmt.Println("Can not unmarshal JSON")
 	}
 
-	client := influxdb2.NewClient(os.Getenv("INFLUXDB_URL"), os.Getenv("INFLUXDB_TOKEN"))
-	writeAPI := client.WriteAPIBlocking(os.Getenv("INFLUXDB_ORG"), os.Getenv("INFLUXDB_BUCKET"))
-	for _, rec := range result {
-		location, locErr := time.LoadLocation("Europe/Stockholm")
-		if locErr != nil {
-			fmt.Println(locErr)
-			return
-		}
+	if debug {
+		fmt.Printf("URL: %s\n", url)
+		fmt.Println(result) // Print the result for debugging
+	} else {
+		client := influxdb2.NewClient(os.Getenv("INFLUXDB_URL"), os.Getenv("INFLUXDB_TOKEN"))
+		writeAPI := client.WriteAPIBlocking(os.Getenv("INFLUXDB_ORG"), os.Getenv("INFLUXDB_BUCKET"))
+		for _, rec := range result {
+			location, locErr := time.LoadLocation("Europe/Stockholm")
+			if locErr != nil {
+				fmt.Println(locErr)
+				return
+			}
 
-		// Parse the timestamp using the Europe/Stockholm time zone
-		date, error := time.ParseInLocation("2006-01-02T15:04:05", rec.TimeStamp, location)
-		if error != nil {
-			fmt.Println(error)
-			return
-		}
+			// Parse the timestamp using the Europe/Stockholm time zone
+			date, error := time.ParseInLocation("2006-01-02T15:04:05", rec.TimeStamp, location)
+			if error != nil {
+				fmt.Println(error)
+				return
+			}
 
-		p := influxdb2.NewPoint("current",
-			map[string]string{"unit": "price"},
-			map[string]interface{}{"last": rec.Value},
-			date)
-		writeAPI.WritePoint(context.Background(), p)
+			p := influxdb2.NewPoint("current",
+				map[string]string{"unit": "price"},
+				map[string]interface{}{"last": rec.Value},
+				date)
+			writeAPI.WritePoint(context.Background(), p)
+		}
+		client.Close()
 	}
-	client.Close()
 	fmt.Printf("%v: Tick\n", t)
 }
 
 func main() {
 	intervalStr := os.Getenv("INTERVAL")
-	interval := 30 // Default interval in minutes
+	interval := 1800 // Default interval in seconds
 	if intervalStr != "" {
 		intervalInt, err := strconv.Atoi(intervalStr)
 		if err == nil {
 			interval = intervalInt
 		} else {
-			fmt.Println("Invalid INTERVAL value, using default 30 minutes")
+			fmt.Println("Invalid INTERVAL value, using default 30 seconds")
 		}
 	}
 
-	doEvery(time.Duration(interval)*time.Minute, pushToInflux)
+	debugStr := os.Getenv("DEBUG")
+	if debugStr != "" {
+		debugBool, err := strconv.ParseBool(debugStr)
+		if err == nil {
+			debug = debugBool
+		} else {
+			fmt.Println("Invalid DEBUG value, should be either true or false, using default false")
+		}
+	}
+
+	doEvery(time.Duration(interval)*time.Second, pushToInflux)
 }
